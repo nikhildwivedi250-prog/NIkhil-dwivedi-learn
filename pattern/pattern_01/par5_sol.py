a560ec2d949dfb3ca93c6483c10d00046e87d52e
Nikhil Dwivedi <nikhildwivedi250@gmail.com>
2026-05-31 07:33:44 +0530

    patt

---
+++ pattern/pattern_01/par5_sol.py
@@ -0,0 +1,20 @@
+n = 6
+
+for i in range(n, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end = " ")
+    print()
+
+
+
+
+
+
+
+
+    #6 5 4 3 2 1 
+    #5 4 3 2 1
+    #4 3 2 1 
+    #3 2 1 
+    #2 1 
+    #1
---
+++ pattern/pattern_01/par7_sol.py
@@ -0,0 +1,15 @@
+num = 4
+
+for i in range(num, 0, -1):
+    for j in range(0, num-i):
+        print(end=" ")
+    for j in range(0, i):
+        print("*", end=" ")
+    print()
+
+
+
+    #* * * *
+     #* * * 
+      #* *
+       #*
