296304ee3466a2fa27b4ee55b209aa2682341c71
Nikhil Dwivedi <nikhildwivedi250@gmail.com>
2026-06-14 15:26:04 +0530

    ulululsldkmfjkk

---
+++ a0/grmini/c&l/ak1.py
@@ -0,0 +1,8 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 33 and 100 >= marks:
+    print("Aap paas ho ")
+elif marks >=  100:
+    print("Abe bakchodi mat kar ")
+else:
+    print("Aap fail ho")
---
+++ a0/grmini/c&l/ak3.py
@@ -0,0 +1,7 @@
+num = int(input("Enter uour number :"))
+
+while num < 0:
+    num = int(input("Enter a positive number number :"))
+
+print(f"your number {num}")
+
