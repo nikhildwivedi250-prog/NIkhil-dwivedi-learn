d2d800f8350767491cc3829a9c408c0447e0374a
Nikhil Dwivedi <nikhildwivedi250@gmail.com>
2026-06-15 16:10:49 +0530

    ggggggggg

---
+++ a0/grmini/c&l/ak4.py
@@ -0,0 +1,7 @@
+game = ["minecraft", "gta5", "hillclimb", "cagames"]
+game.append("freefire")
+game.append("busindonesiya")
+game.append("visecity")
+game.append("pubg")
+game.append("carsimulator")
+print(game)
---
+++ a0/grmini/c&l/ak7.py
@@ -0,0 +1,6 @@
+def add_item(my_list):
+    my_list.append("daimond")
+
+inventory = ["Sword"]
+add_item(inventory)
+print(inventory)
