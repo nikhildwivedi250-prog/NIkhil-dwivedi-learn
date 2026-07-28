fe576ea486b26c5510ff396b93195f1e15a8a44f
Nikhil Dwivedi <nikhildwivedi250@gmail.com>
2026-07-28 19:47:41 +0530

    quct

---
+++ question by ct/conditions/largeof2num.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number :- "))
+b = int(input("Enter second number :- "))
+
+
+if a > b:
+    print("Large: ", a)
+else:
+    print("Large: ", b)
+    
---
+++ question by ct/conditions/vote_eligibal.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age :- "))
+
+if age >= 18 :
+    print("Eligibal for voting")
+else:
+    print("Not Eligibal")
